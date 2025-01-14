12c3467d05e4971d82cc408e2317c23b2d08c6eb
lemonpeppereel <elijahpark01@gmail.com>
2025-01-14 12:50:00 -0500

    database connected

--- AppServer/database/db_connection.js
+++ AppServer/database/db_connection.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 const connection = mysql.createConnection ({
     host: 'sqlclassdb-instance-1.cqjxl5z5vyvr.us-east-2.rds.amazonaws.com',
+    port: 3306,
     user: 'elijah',
     password: '7YavdDBvqLZE',
-    database: 'midyear_project_2025'
+    database: 'mid_year_project'
 })
 
 module.exports = connection;
